df74172625d96fb8d51927a0be2e5ee43247ecbc
MuhammedAlaa <soupermado@gmail.com>
2019-10-05 15:56:03 +0200

    fix conflict

--- app/src/main/java/com/example/firebase/DisplayTaskActivity.kt
+++ app/src/main/java/com/example/firebase/DisplayTaskActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_display_event.*
+import kotlinx.android.synthetic.main.activity_display_task.*
 
 class DisplayTaskActivity : AppCompatActivity() {
 
@@ -16,17 +18,10 @@ class DisplayTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_task)
-        topic = intent.getStringExtra("Topic")
-        details = intent.getStringExtra("Details")
-        deadlinedate = intent.getStringExtra("DeadLine")
-        target = intent.getStringExtra("Target")
-        val topicTxt = findViewById<TextView>(R.id.TOPIC)
-        val detailsTxt = findViewById<TextView>(R.id.DETAILS)
-        val deadlineTxt = findViewById<TextView>(R.id.Deadline)
-        val  targetTxt = findViewById<TextView>(R.id.Target)
-        topicTxt.text = topic
-        detailsTxt.text = details
-        deadlineTxt.text = deadlinedate
-        targetTxt.text = target
+        TOPIC.text = intent.getStringExtra("Topic")
+        DETAILS.text = intent.getStringExtra("Details")
+        Deadline.text = intent.getStringExtra("DeadLine")
+        Target.text = intent.getStringExtra("Target")
+
     }
 }
